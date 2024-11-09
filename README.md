4f7db6afef146ad0c2ae279fe8ccde20eda84c59
Zakum <mirveo@gmail.com>
2024-11-08 19:57:57 -0800

    update characters logic

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ idk: `/Marvel/Content/Marvel/Data/DataTable/MarvelShopTable.uasset`
 
 hero headshot: `/Marvel/Content/Marvel/UI/Textures/HeroPortrait/SelectHero/img_selecthero_1029101.uasset`
 hero portrait: `/Marvel/Content/Marvel/UI/Textures/Show/Skin/OriginalSkin/img_heroportrait_10160010_portrait.uasset`
+
+StringTables seem to exist inside of Game.locres. For example, 601_HeroUIAsset_1015_ST.json's data is found inside the Game.locres object "601_HeroUIAsset_1015_ST". Some files also have SourceString and LocalizedString included in them, like UIHeroTable, though it's unknown which source is the correct source or whether they are always the same source and are just added in to their respective files.
--- src/lib/characters.js
+++ src/lib/characters.js
@@ -1,32 +1,45 @@
 import UIHeroTable from "../../game/client/Content/Marvel/Data/DataTable/HeroGallery/UIHeroTable.json"
 import { imgPath } from "./utils"
+import locres from "../../game/client/Content/Localization/Game/zh-Hans/Game.json"
 
 // TODO: Team-up abilities (HeroBond/Collab?)
 // TODO: Character skills
 
-export default Object.entries(UIHeroTable[0].Rows).map(([key, shapeData]) => {
-    let id, shapeId, name, desc, icon, enName, difficulty, color, role, somatotype, categories;
+// Each object in UIHeroTable is a "Shape". Shape refers to a variant of a hero, i.e. Bruce Banner, Hero Hulk, Monster Hulk. Few characters have more than one shape.
+function transformCharacterData([key, shapeData]) {
+    let hero = {}
 
     for (const key in shapeData) {
-        if (key.startsWith("HeroID_")) id = shapeData[key]
-        if (key.startsWith("ShapeID_")) shapeId = shapeData[key]
-        if (key.startsWith("HeroName_")) name = shapeData[key]
+        const value = shapeData[key]
+        if (key.startsWith("HeroID_")) hero.id = value
+        if (key.startsWith("ShapeID_")) hero.shapeId = value
+        if (key.startsWith("HeroName_")) hero.heroName = value
+
         if (key.startsWith("HeroBasic_")) {
-            for (const deepKey in shapeData[key]) {
-                if (deepKey.startsWith("EnName_")) enName = shapeData[key][deepKey]
-                if (deepKey.startsWith("Role_")) role = shapeData[key][deepKey].split("::").pop()
-                if (deepKey.startsWith("Somatotype_")) somatotype = shapeData[key][deepKey].split("::").pop()
-                if (deepKey.startsWith("Team_")) categories = shapeData[key][deepKey].map(cat => cat.split("::").pop())
-                if (deepKey.startsWith("Difficult_")) difficulty = shapeData[key][deepKey]
-                if (deepKey.startsWith("Desc_")) desc = shapeData[key][deepKey].LocalizedString // Might be incorrect; check against the TableId and Key values.
-                if (deepKey.startsWith("HeroInfoMainColor_")) color = `#${shapeData[key][deepKey].Hex}`
+            for (const deepKey in value) {
+                const deepValue = value[deepKey]
+                if (deepKey.startsWith("RealName_")) {
+                    const { TableId, Key } = deepValue
+                    if (TableId) {
+                        const ns = TableId.split(".").pop()
+                        hero.realName = locres[ns][Key]
+                    }
+                }
+                if (deepKey.startsWith("EnName_")) hero.enName = deepValue
+                if (deepKey.startsWith("Role_")) hero.role = deepValue.split("::").pop()
+                if (deepKey.startsWith("Somatotype_")) hero.somatotype = deepValue.split("::").pop()
+                if (deepKey.startsWith("Team_")) hero.categories = deepValue.map(cat => cat.split("::").pop())
+                if (deepKey.startsWith("Difficult_")) hero.difficulty = deepValue
+                if (deepKey.startsWith("Desc_")) hero.desc = deepValue.LocalizedString // Might be incorrect; check against the TableId and Key values.
+                if (deepKey.startsWith("HeroInfoMainColor_")) hero.color = `#${deepValue.Hex}`
             }
         }
+
         if (key.startsWith("HeroHead_")) {
-            for (const deepKey in shapeData[key]) {
+            for (const deepKey in value) {
                 if (deepKey.startsWith("HeroHeadBig_")) {
-                    for (const deeperKey in shapeData[key][deepKey]) {
-                        if (deeperKey.startsWith("Image_")) icon = imgPath(shapeData[key][deepKey][deeperKey].AssetPathName)
+                    for (const deeperKey in value[deepKey]) {
+                        if (deeperKey.startsWith("Image_")) hero.icon = imgPath(value[deepKey][deeperKey].AssetPathName)
                     }
                 }
             }
@@ -34,19 +47,25 @@ export default Object.entries(UIHeroTable[0].Rows).map(([key, shapeData]) => {
         // Skill icon when hero bond is activated: HeroBond_112_153B26FB4FB57DFDEFD2BDA1EBCE8B80
     }
 
-    return {
-        key,
-        id,
-        name,
-        shapeId,
-        icon,
-        desc, 
-        enName, 
-        difficulty, 
-        color, 
-        role, 
-        somatotype, 
-        categories,
-        // ...shapeData
-    }
-})
+    hero.shapeKey = key
+
+    return hero
+}
+
+// Only get UNIQUE characters by moving variant forms (Hero Hulk, Monster Hulk) into the original hero (Bruce Banner) data.
+const uniqueCharacters = Object.entries(UIHeroTable[0].Rows)
+    .reduce((acc, rawData) => {
+        const character = transformCharacterData(rawData)
+        if (!acc[character.id]) {
+            acc[character.id] = character
+        }
+        else {
+            if (!acc[character.id].variants) acc[character.id].variants = []
+            acc[character.id].variants.push(character)
+        }
+        return acc
+    }, {})
+
+const characters = Object.values(uniqueCharacters)
+
+export default characters
