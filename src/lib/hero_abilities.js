7b91532919c8bed956ab8491781371018fc370bd
Zakum <mirveo@gmail.com>
2024-11-11 08:18:28 -0800

    Update hero_abilities.js

--- src/lib/hero_abilities.js
+++ src/lib/hero_abilities.js
@@ -14,23 +14,82 @@ export default abilitiesFiles.flatMap(([path, data]) => {
 
     const abilitiesInFile = Object.values(data[0].Rows)
         .map(ability => {
+            const {
+                AbilityID,
+                HeroName,
+                TName,
+                ActivateInput,
+                AbilityCD,
+                AbilityCDID, // not sure how this is used; maybe for CDs that can be reset/reduced with certain actions such as Psylocke
+                AbilityAsset, // links to the ability's specific file, i.e. 101102.json
+                AbilityTags, // "Jump", "Bond", "Passive" tags etc.
+                bSaveCDInPlayerState,
+                bSharedCDInTeam,
+                bKeepCDWhenReborn,
+                MaxChargeCount,
+                ChargeAutoRemove, // unsure
+                ChargeInterval,
+                bCacheSkill,
+                QuoteBuff, // some sort of object for buffs?
+                Description,
+                AbilityLogTags, // "UltimateAbility" tags etc.
+                AbilityRedirectLog, // Career stat logging (Gamma Storm total kills etc.)
+                AbilityLog, // Career stats (ZiplineSlideDistance, RespawnCnt)
+                BuffLog, // list of buffs it applies? just has BuffID
+                ForbiddenAbilityBaseLog, // unsure, always empty
+                ToRecordEffectIDs, // some sort of logging "EffectType": "EMarvelEffectType::Scope"
+                AbilityStatisticsClass, // stats tracking? folder doesnt exist in fmodel
+                AbilityExtraStatisticsClass, // stats tracking? folder doesnt exist in fmodel
+                DamageTypeClass, // Low or High destruction damage
+                ImpulseLevelInfo, // unsure
+                ImpulseApplyFilter, // unsure
+                BeHitAnimConfigID, // unsure
+                AbilityUITags, // Normal, Melee, Weapon, Ultimate
+            } = ability
+
+            let name = null;
+            if (TName.TableId && locres[sp(TName.TableId, ".")]) {
+                name = locres[sp(TName.TableId, ".")][TName.Key]
+            }
+
+            let desc = null;
+            if (Description.TableId && locres[sp(Description.TableId, ".")]) {
+                desc = locres[sp(Description.TableId, ".")][Description.Key]
+            }
+
             return {
                 heroId,
-                ...mapKeys(ability, {
-                    ActivateInput: "input",
-                    AbilityCD: "cooldown",
-                    AbilityCDID: "cooldownId",
-                    // AbilityAsset links to the ability file, i.e. 101102.json
-                    AbilityTags: "tags",
-                    bSaveCDInPlayerState: "isCooldownSavedToPlayerState",
-                    bSharedCDInTeam: "isTeamSharedCooldown",
-                    bKeepCDWhenReborn: "isMaintainCooldownAfterRevive",
-                    MaxChargeCount: "maxCharges",
-                    ChargeAutoRemove: "chargeAutoRemove", // ??
-                    ChargeInterval: "chargeInterval",
-                    TName: ["name", ({ TableId, Key }) => TableId && locres[sp(TableId, ".")] ? locres[sp(TableId, ".")][Key] : null]
-                }),
-                ...ability
+                name,
+                desc,
+                AbilityID,
+                HeroName,
+                ActivateInput,
+                AbilityCD,
+                AbilityCDID,
+                AbilityAsset,
+                AbilityTags,
+                bSaveCDInPlayerState,
+                bSharedCDInTeam,
+                bKeepCDWhenReborn,
+                MaxChargeCount,
+                ChargeAutoRemove,
+                ChargeInterval,
+                bCacheSkill,
+                QuoteBuff,
+                AbilityLogTags,
+                AbilityRedirectLog,
+                AbilityLog,
+                BuffLog,
+                ForbiddenAbilityBaseLog,
+                ToRecordEffectIDs,
+                AbilityStatisticsClass,
+                AbilityExtraStatisticsClass,
+                DamageTypeClass,
+                ImpulseLevelInfo,
+                ImpulseApplyFilter,
+                BeHitAnimConfigID,
+                AbilityUITags,
+                // ...ability
             }
         })
 
