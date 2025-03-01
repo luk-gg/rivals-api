87a2cff0555e30e0f22259c85d8e5a52dbb8881b
Zakum <mirveo@gmail.com>
2025-03-01 08:08:16 -0800

    add api-utils submodule

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,32 +1,3 @@
-import fs from "fs"
-
-// Uses linear interpolation to incrementally add missing values to an UnrealEngine array of [{ Time, Value }] objects and return them as [{ x, y }]
-export function RCIM_Linear(arr) {
-    return arr.reduce((acc, curr, index) => {
-        let { Time: x0, Value: y0 } = curr
-        const { Time: x1, Value: y1 } = arr[index + 1] || {}
-
-        // Push the current value from the original array
-        acc.push({ x: x0, y: y0 })
-
-        // Fill in missing values from the current Time to the next Time
-        let missingX = x0
-        while (x1 > missingX + 1) {
-            missingX++
-            const missingY = y0 + (y1 - y0) * ((missingX - x0) / (x1 - x0))
-            acc.push({ x: missingX, y: missingY })
-        }
-
-        return acc
-    }, [])
-}
-
-export function imgPath(path) {
-    if (!path) return
-    return path.replace("/Game", "/Content").split(".")[0] + ".png"
-}
-
-// Shrinks each entries[] object to only have the necessary keys for displaying in a list, i.e. name and icon.
 export function getBriefArr(arr) {
     return arr.map(data => getBriefData(data))
 }
@@ -36,17 +7,7 @@ export function getBriefData(fullData) {
     return { id, name, icon }
 }
 
-// Ensure case-insensitivity as svelte's routing (or the browser?) transforms links like /Characters/UCR001 to /characters/ucr001. 
-export function writeJson(dir, fileName, data) {
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-    fs.writeFileSync(`${dir}/${fileName.toString().toLowerCase()}.json`, JSON.stringify(data))
-}
-
-// Split-pop function for removing Unreal Engine variable types, i.e. "ESkillType::Attack" → "Attack".
-export const sp = (str, delimiter = "::") => str.split(delimiter).pop()
-
+// MAYBE MOVE THIS TO API-UTILS
 // Take an original uasset object where the keys include random numbers, and find the data and assign it to the new keys provided in the object "maps". 
 // HeroID_: "id" → find the key that starts with "HeroID_" and return its value under key "id"
 // HeroName_: ["heroName", (name) => name + " Hello"] → find the key that starts with "HeroName_" and return its value under key "heroName" after appending " Hello" to the end.
